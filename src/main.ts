bd2aaa36da72cdabb8e59a9123aad97aaba77d5a
Healer101015 <josonjonesrids@gmail.com>
2026-04-17 11:55:47 -0300

    teste4

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Blog Pessoal')
-    .setDescription('Projeto Blog Pessoal')
-    .setContact("O seu Nome", "http://www.seugithub.com", "seuemail@email.com")
+    .setTitle('CRM Master')
+    .setDescription('Projeto CRM Master')
+    .setContact("O seu Nome", "https://github.com/Healer101015/crm_master", "seuemail@email.com")
     .setVersion('1.0')
     .addBearerAuth()
     .build();
