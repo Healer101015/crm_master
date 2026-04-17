47a9012bd4bd184fb49b746245ddaa6195f9b81d
Healer101015 <josonjonesrids@gmail.com>
2026-04-17 11:40:29 -0300

    teste2

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,11 +7,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: 'secret',
+            ignoreExpiration: false,
+            // AQUI ESTÁ A CORREÇÃO: Tem que buscar a variável do Render igual no módulo!
+            secretOrKey: process.env.JWT_SECRET || 'secret',
         });
     }
 
     async validate(payload: any) {
-        return payload;
+        // Retorna os dados do usuário extraídos do token
+        return { id: payload.sub, email: payload.username };
     }
 }
